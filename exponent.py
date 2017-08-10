3d9fd3239dbde4f17ff6b40b898912cc00963ec6
nimbejuarez <nimbe.juarez@avant.com>
2017-08-10 12:56:26 -0500

    function_and_loops

---
+++ exponent.py
@@ -0,0 +1,4 @@
+user_input = input("Enter a base:")
+user_input2 = input("Enter an exponent:")
+power_answer = float(user_input)**float(user_input2)
+print("{0} to the power of {1} = {2}".format(user_input, user_input2, power_answer))
